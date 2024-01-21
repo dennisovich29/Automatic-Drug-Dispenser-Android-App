593338b3e20f0523ca8df8e90eb6d9fcc91ef999
dennisovich29 <157195198+dennisovich29@users.noreply.github.com>
2024-01-21 16:40:54 +0530

    Update PatientRoutes.js
    added QR code

--- Routing/PatientRoutes.js
+++ Routing/PatientRoutes.js
@@ -4,8 +4,10 @@ const express=require("express")
 const jwt = require("jsonwebtoken")
 
 const router = express.Router()
-
+const medicine =require("../Models/medecinModel")
 const patient = require("../Models/patientModel")
+const prescription = require("../Models/DocprescriptionModel")
+const qrcode = require("qrcode")
 const {authenticateTokenPatient} = require("../Middleware/authenticateToken")
 
 // generating a 6 digit number using math 
@@ -30,7 +32,7 @@ router.post("/signup",async(req,res) => {
         //create new patient 
         const newPatinet=new patient({name,phone_no,password,uniqueId:generateRandomNumber()})
         await newPatinet.save()
-        res.status(200).json({message: "User registration successful!!.."})
+        res.status(200).json({message: "User registration successful!!.. "})
 
     } catch (error) {
         res.status(500).json({error:"Internal Server Error",details:error.message})
@@ -89,19 +91,19 @@ router.get('/profile', authenticateTokenPatient, async (req, res) => {
     const userId = req.patient.userId
     try {
         // Find the patient by userId
-        const foundPatient = await patient.findById(userId);
+        const foundPatient = await patient.findById(userId)
 
         if (!foundPatient) {
-            return res.status(404).json({ error: 'Patient not found' });
+            return res.status(404).json({ error: 'Patient not found' })
         }
         const {name,uniqueId,phone_no}=foundPatient
         const extractedData = {name,uniqueId,phone_no}
         // Respond with the patient details
-        res.status(200).json(extractedData);
+        res.status(200).json(extractedData)
     } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error', details: error.message });
+        res.status(500).json({ error: 'Internal Server Error', details: error.message })
     }
-});
+})
 
 // displaying only the uniqueId in the home section 
 
@@ -109,23 +111,120 @@ router.get('/home', authenticateTokenPatient, async (req, res) => {
     const userId = req.patient.userId
     try {
         // Find the patient by userId
-        const foundPatient = await patient.findById(userId);
+        const foundPatient = await patient.findById(userId)
 
         if (!foundPatient) {
-            return res.status(404).json({ error: 'Patient not found' });
+            return res.status(404).json({ error: 'Patient not found' })
         }
         const {uniqueId}=foundPatient
         const extractedData = {uniqueId}
         // Respond with the patient details
-        res.status(200).json(extractedData);
+        res.status(200).json(extractedData)
     } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error', details: error.message });
+        res.status(500).json({ error: 'Internal Server Error', details: error.message })
     }
-});
+})
+
+// medicine search suggestion 
+router.get('/selfPrescription/search', async (req, res) => {
+    const { query } = req.body
+  
+    try {
+      // Use Mongoose's text search for basic case-insensitive search
+      const suggestions = await medicine.find(
+        { name: { $regex: new RegExp(`^${query}`, 'i') } } ,
+        { _id: 0, name: 1, mg: 1 },
+      ).limit(5) // Limit the number of suggestions
+  
+      res.status(200).json({ suggestions })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ error: 'Internal Server Error ' ,details:error.message})
+    }
+})
+  
+
+// self prescription
+
+router.post('/selfPrescription',authenticateTokenPatient, async (req, res) => {
+    const userId = req.doctor.userId
+    const { medicines: medicineDetails } = req.body
+  
+    try {
+        const currentDoc = await doctor.findById(userId)
+        const patientFound = await patient.findOne({uniqueId})
+        if (!patientFound) {
+            return res.status(404).json({ error: 'Patient not found' })
+        }
+        
+        const newPrescription = {
+            sent_to:{name: patientFound.name,uniqueId:patientFound.uniqueId},
+            sent_by:{name:currentDoc.name,registrationNumber:currentDoc.registrationNumber},
+            Medicines: [],
+            sent: true,
+        }
+    
+        for (const medicineDetail of medicineDetails) {
+            const { name, mg, quantity } = medicineDetail
+    
+            const Medicine = await medicine.findOne({ name, mg })
+            if (Medicine) {
+                newPrescription.Medicines.push({
+                    Medicine: Medicine.name,
+                    mg:Medicine.mg,
+                    quantity,
+                    price:Medicine.price * quantity
+                })
+            }
+            else {
+              // If the medicine is not found, you might want to handle it (e.g., return an error)
+              return res.status(404).json({message:"Given medicine not found"})
+            }
+        }
+    
+        const prescriptionInstance = new prescription(newPrescription)
+        const savedPrescription = await prescriptionInstance.save()
+    
+        res.status(200).json({ message: 'Medicines added to prescription successfully', prescription: savedPrescription })
+    } catch (error) {
+    //   console.error(error)
+      res.status(500).json({ error: 'Internal Server Error' ,details:error.message})
+    }
+})
 
 
-// view the sent prescription 
+// generate qr 
+
+router.get('/generateQR/:prescriptionId', async (req, res) => {
+    const { prescriptionId } = req.params
+
+    try {
+        const savedPrescription = await prescription.findById(prescriptionId)
+        if (!savedPrescription) {
+            return res.status(404).json({ error: 'Prescription not found' })
+        }
+
+        // Extracting relevant data for the QR code
+        const qrData = savedPrescription.Medicines.map(medicine => ({
+            name: medicine.Medicine_name,
+            mg: medicine.mg,
+            quantity: medicine.quantity,
+        }))
+
+        // Generate QR code image
+        const qrImage = await qrcode.toDataURL(JSON.stringify(qrData))
+
+        res.status(200).json({ message: 'QR code generated successfully', qrImage })
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error', details: error.message })
+    }
+})
 
 
+// to get all medicines 
+router.get('/medicines',async(req, res) => {
+    const allMed= await medicine.find({})
+    res.json({ allMed })
+})
 
 module.exports = router 
