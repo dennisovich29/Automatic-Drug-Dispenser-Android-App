3959e32daac89dab6a6f0d706a288e4006b720b3
dennisovich29 <157195198+dennisovich29@users.noreply.github.com>
2024-01-21 16:57:09 +0530

    Add files via upload

---
+++ Models/DocprescriptionModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+
+const prescriptionSchema = new mongoose.Schema({
+  prescriptionId:mongoose.Schema.Types.ObjectId,
+  sent_to :({name:{type:String},uniqueId:{type:Number}}),
+  sent_by :({name:{type:String},registrationNumber:{type:Number}}),
+  date:{type:Date,default:Date.now},
+    
+  Medicines: [{
+      Medicine_name:{type:String},
+      mg:{type:Number},
+      quantity: {type:Number},
+      price:{type:Number}
+  }],
+
+  sent: { type: Boolean, default: false },
+})
+
+const prescription = mongoose.model("prescription",prescriptionSchema)
+
+module.exports = prescription
