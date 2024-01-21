139b86ff9a50c156c2321848df6d03ea949687cb
dennisovich29 <157195198+dennisovich29@users.noreply.github.com>
2024-01-21 16:46:22 +0530

    Update app.js
    up to date

--- app.js
+++ app.js
@@ -16,137 +16,17 @@ app.use("/doctor",doctorRoutes)
 async function startServer() {
   try {
     // Connect to MongoDB
-    await mongoose.connect(uri);
+    await mongoose.connect(uri)
 
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB')
 
     // Start the server
     app.listen(port, () => {
-      console.log(`Server started at http://localhost:${port}`);
+      console.log(`Server started at http://localhost:${port}`)
     });
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error('Error connecting to MongoDB:', error)
   }
 }
 // Call the async function to start the server
-startServer();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// mongoose.connect(docuri)
-// // mongoose.connect(docuri)
-// .then(() => {
-//     app.listen(8000,(res,req) => {
-//         console.log("server started at port 8000 .... ")
-//     })
-//     console.log("connected....to mongoDB")
-// }).catch((error) => {
-//     res.status(500).json({message:message.error,details:error.message})
-//     process.exit(1);
-// })
-
-
-
-
-// const medicines = require("./MODELS/medecinModel")
-
-// // routing 
-
-// // retreving the whole medicine list 
-// app.get("/medicines",async(req,res) => {
-//     try{
-//         const med = await medicines.find({})
-//         res.status(200).json(med)
-//     }
-//     catch(error){
-//         res.status(500).json({message: message.error})
-//     }
-// })
-
-// //retreving one single tablet from the data base
-// app.get("/medicines/:id",async(req,res) => {
-//     try{
-//         const {id}=req.params
-//         const med = await medicines.findById(id)
-//         res.status(200).json(med)
-//     }
-//     catch(error){
-//         res.status(500).json({message: message.error})
-//     }
-// })
-
-// //update the data base by one tablet 
-// app.put("/medicines/:id",async(req,res) => {
-//     try {
-//         const {id}=req.params
-//         const med = await medicines.findByIdAndUpdate(id,req.body)
-//         if(!med){
-//             return res.status(404).json({message: `cant find medicine by the id ${id}`})
-//         }
-//         res.status(200).json(med)
-
-//     } catch (error) {
-//         res.status(500).json({message: message.error})
-//     }
-// })
-
-// // delete the tablet from the data base
-// app.delete("/medicines/:id",async(req,res) => {
-//     try {
-//         const {id} = req.params
-//         const med= await medicines.findByIdAndDelete(id)
-//     } catch (error) {
-//         res.status(500).json({message: message.error})
-//     }
-// })
-
-// // adding a medicine into database
-// app.post("/medicines",async(req,res) => {
-//     try{
-//         const med = await medicines.create(req.body)
-//         res.status(200).json(med)
-    
-//     }catch(error){
-//         console.log(error)
-//         res.status(500).json({message: error.message})
-//     }
-// })
-
-
-// // adding a docotor into database 
-// app.post("/patient",async(req,res) => {
-//     try{
-//         const med = await doctors.create(req.body)
-//         res.status(200).json(med)
-    
-//     }catch(error){
-//         console.log(error)
-//         res.status(500).json({message: error.message})
-//     }
-// })
+startServer()
