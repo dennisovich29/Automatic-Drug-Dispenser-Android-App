c1c90deb9aa6f88c59e67cb642341842c0724782
dennisovich29 <157195198+dennisovich29@users.noreply.github.com>
2024-01-21 16:44:50 +0530

    Update prescriptionModel.js
    up to date

--- Models/prescriptionModel.js
+++ Models/prescriptionModel.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose")
-const medicine = require("./medecinModel")
-const patient = require("./patientModel")
-const { Double } = require("mongodb")
 
 
 const prescriptionSchema = new mongoose.Schema({
-    Medicines: [{
-        Medicine:{type:String},
-        mg:{type:Number},
-        quantity: {type:Number},
-        price:{type:Number}
-      }],
-    sent_to :({name:{type:String},uniqueId:{type:Number}}),
-    sent_by :({name:{type:String},registrationNumber:{type:Number},specialization:{type:String}}),
-    date:{type:Date,default:Date.now},
-    sent: { type: Boolean, default: false },
+  prescriptionId:mongoose.Schema.Types.ObjectId,
+  sent_to :({name:{type:String},uniqueId:{type:Number}}),
+  sent_by :({name:{type:String},registrationNumber:{type:Number}}),
+  date:{type:Date,default:Date.now},
+    
+  Medicines: [{
+      Medicine_name:{type:String},
+      mg:{type:Number},
+      quantity: {type:Number},
+      price:{type:Number}
+  }],
+
+  sent: { type: Boolean, default: false },
 })
 
 const prescription = mongoose.model("prescription",prescriptionSchema)
